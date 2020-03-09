14827e1792f1ff86a4b5bca4ae87c5a7e3268b17
hitesh <hiteshsp@outlook.com>
2020-03-09 02:36:28 +0000

    Typo in forms

--- url_shortener/forms.py
+++ url_shortener/forms.py
@@ -3,10 +3,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 
 
-class UrlForm(FlaskForm):
-    """
-        Creates two elements for the long_url and a submit button
-    """
+class URLForm(FlaskForm):
     long_url = StringField('URL', [DataRequired(), URL(
         message="Must be a valid URL with http:// or https://")])
     submit = SubmitField('Shorten iT!')
