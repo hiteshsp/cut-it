594a577acb026a356bdbc16886720ba4fb6faa19
Hitesh <hiteshsp@outlook.com>
2020-03-12 12:24:31 +0530

    Minor Typo in run.py

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
 from url_shortener import app
 
-<<<<<<< HEAD
-# Entry Point of the program
-app.run(host='0.0.0.0', port=5001, debug=True)
-=======
 app.run(host='0.0.0.0', port=5000, debug=True)
 
->>>>>>> origin/develop
